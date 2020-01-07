2a7a3c5dd0f23bac36d5329dcf65c95c5b3f4692
Valentin Burkin <burkinman@me.com>
2020-01-07 19:55:13 +0300

    feat: Add run_kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=valentinburk/flask-prediction:0.1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flask-prediction --image=${dockerpath} --labels="tag=udacity"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+POD_NAME=$(kubectl get pods -l tag=udacity -o jsonpath='{.items[0].metadata.name}')
+kubectl port-forward "${POD_NAME}" 8000:80
