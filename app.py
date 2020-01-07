8bfdcc807bea0834e2565b09d15120a8bed13617
Valentin Burkin <burkinman@me.com>
2020-01-07 19:24:42 +0300

    feat: add scripts to build and publish docker image
    Additionally, fix the issue with sklearn wrong version
    which prevents python app from making predictions properly
    and causes the 500 status code response for each request

--- app.py
+++ app.py
@@ -6,9 +6,16 @@ import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
+TITLE = "Sklearn Prediction Home"
+HOST = "0.0.0.0"
+PORT = 80
+DEBUG = True
+DATA_FILE = "./model_data/boston_housing_prediction.joblib"
+LOG_LEVEL = logging.INFO
+
 app = Flask(__name__)
 LOG = create_logger(app)
-LOG.setLevel(logging.INFO)
+LOG.setLevel(LOG_LEVEL)
 
 def scale(payload):
     """Scales Payload"""
@@ -16,11 +23,12 @@ def scale(payload):
     LOG.info(f"Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
+
     return scaled_adhoc_predict
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = f"<h3>{TITLE}</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -56,16 +64,20 @@ def predict():
     # Logging the input payload
     json_payload = request.json
     LOG.info(f"JSON payload: \n{json_payload}")
+
     inference_payload = pd.DataFrame(json_payload)
     LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+
     # scale the input
     scaled_payload = scale(inference_payload)
-    # get an output prediction from the pretrained model, clf
+    LOG.info(f"Scaled payload: \n{scaled_payload}")
+
     prediction = list(clf.predict(scaled_payload))
     LOG.info(f"Prediction: \n{prediction}")
+
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
     # load pretrained model as clf
-    clf = joblib.load("./model_data/boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+    clf = joblib.load(DATA_FILE)
+    app.run(host=HOST, port=PORT, debug=DEBUG)
--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag valentinburk/flask-prediction:0.1 .
 
 # Step 2: 
 # List docker images
+docker images --all
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 valentinburk/flask-prediction:0.1
